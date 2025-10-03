9a98e9c70b0d2651bf5170c59d0f516d4a954db1
duy4nt <singhduyant@gmail.com>
2025-10-03 09:30:30 +0000

    Improving the modularity of code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("searching for: {query}");
+    println!("in file: {file_path}");
+
+    let content = fs::read_to_string(file_path).expect("should have been able to read the file");
+    println!("with text: \n{content}");
+
+    
 }
