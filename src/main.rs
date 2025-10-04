63422701428e3e55bf13c1102529cbce92aabb45
duy4nt <singhduyant@gmail.com>
2025-10-04 03:23:57 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-    
-    let query = &args[1];
-    let file_path = &args[2];
+ 
+    let config = parse_config(&args);
 
-    println!("searching for: {query}");
-    println!("in file: {file_path}");
+    println!("searching for: {}", config.query);
+    println!("in file: {}", config.file_path);
 
-    let content = fs::read_to_string(file_path).expect("should have been able to read the file");
+    let content = fs::read_to_string(config.file_path).expect("should have been able to read the file");
     println!("with text: \n{content}");
 
     
+}
+
+struct Config{
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config(query, file_path)
 }
