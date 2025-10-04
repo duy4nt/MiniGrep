938226c3f772000311d6f47082ca854063154460
duy4nt <singhduyant@gmail.com>
2025-10-04 16:52:47 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
+use minigrep::search;
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
- 
-    let config = parse_config(&args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("searching for: {}", config.query);
     println!("in file: {}", config.file_path);
 
-    let content = fs::read_to_string(config.file_path).expect("should have been able to read the file");
-    println!("with text: \n{content}");
-
-    
+    if let Err(e) = run(config) {
+        println!("application error: {e}");
+        process::exit(1);
+    };
 }
 
-struct Config{
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+struct Config {
     query: String,
     file_path: String,
 }
@@ -25,5 +41,17 @@ fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
 
-    Config(query, file_path)
+    Config { query, file_path }
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
